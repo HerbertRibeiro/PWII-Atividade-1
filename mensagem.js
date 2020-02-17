7b6e382a6a3ab284a5e5675abcf24ebc78714e32
HebertRibeiro <herbert.spgs1308@gmail.com>
2020-02-17 17:24:56 -0300

    Atualização de  banner ao abrir a página

--- mensagem.js
+++ mensagem.js
@@ -1,6 +1,6 @@
 var banners = ["Os melhores do Brasil!"," Qualidade e preço baixo!"];
 var bannerAtual = 0;
-function trocaBanner() {
+function trocaBanner(){ 
  bannerAtual = (bannerAtual + 1) % 2;
  document.querySelector(’h2#mensagem’).textContent = banners[bannerAtual];
 }
